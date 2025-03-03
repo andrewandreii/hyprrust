3abffa0ab63e6c95f232527b84dba1e52ac0524f
andrewandreii <andrei3f@yahoo.com>
2025-03-03 10:33:56 +0200

    docs: add crate docs and update readme

--- README.md
+++ README.md
@@ -1,11 +1,21 @@
 # Hyprrust
 
-A rust library for communicating with the Hyprland sockets.
+A crate that provides a rust interface to communicate with the Hyprland sockets.
 
 While there are other crates that are used to communicate with the Hyprland sockets, this one might stand out because of:
- - Events are received through async channels
- - Ability to connect to as many hyprland sockets as you want
+ - Ability to connect to more hyprland sockets/instances
  - Ability to implement your own Hyprland commands
+ - Events can be received through an async channel
  - Batching commands
+ - Filtering commands (both exclusion and inclusion filters)
 
-This library hasn't released yet.
+### Get started
+
+To start, add this crate to your project:
+```sh
+cargo add hyprrust
+```
+
+Look at the [examples](./examples).
+
+And read the documentation.
--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,39 @@
+//! A crate that provides a rust interface to communicate with the Hyprland sockets.
+//!
+//! There are two types of Hyprland sockets, one used for events and one used for commands or to
+//! request data (ie. clients, decorations, etc.)
+//!
+//! The main struct of this crate is [`HyprlandConnection`]. It's used for connecting to both
+//! sockets (except when you need sync events, in which case you should use [`DetachedEventConnection`]).
+//!
+//! [`DetachedEventConnection`]: crate::events::single_event_sync::DetachedEventConnection
+//!
+//! # Crate overview
+//!
+//! ### Get started
+//!
+//! By default, this crate enables both the `sync` and `async` features. It's preferred to disable
+//! default features and include only the `sync` feature if you don't need `async`, this way
+//! `tokio` is not included.
+//!
+//! To get started add this crate to your project:
+//! ```sh
+//! cargo add hyprrust
+//! ```
+//!
+//! ### Module structure
+//!
+//! This crate conatins several modules:
+//!  - `commands`: All implemented commands, but doesn't include arguments.
+//!  - `commands::prelude`: All commands and arguments.
+//!  - `data`: All commands used to request data from Hyprland.
+//!  - `errors`: All errors returned by the API of this library.
+//!  - `events`: Contains everything needed to receive sync and async events and filter them.
+//!
+//! ### Examples
+//!
+//! See the `examples` folder in the crates source.
+
 mod connection;
 mod ctl;
 pub mod errors;
